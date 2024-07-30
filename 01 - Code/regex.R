dc154d04e00a4714a91b6e6643899b9e4ddb8e06
Hugo Marthinet <h.marthinet@gmail.com>
2024-07-30 09:26:08 +0200

    Added some regex examples

--- 01 - Code/regex.R
+++ 01 - Code/regex.R
@@ -2,10 +2,16 @@
 gsub("[0-9]+", "", my_string)
 
 
-# Nombre de fois qu'un caractere apparit dansn une chaine de caracteres
+# Nombre de fois qu'un caractere apparait dans une chaine de caracteres
 seq <- "ABABBCBD"
 vi <- rep(NA, nchar(seq))
 for (i in 1:nchar(seq)) {
   vi[i] <- length(lapply(gregexpr(substr(seq, i, i), seq), attributes)[[1]]$match.length)
 }
 vi
+
+
+# Tous les caractères AVANT l'occurence du premier espace
+sub("\\ .*", "", x)
+# Tous les caractères APRES l'occurence du premier espace
+sub("^[^_]* ", "", x)
