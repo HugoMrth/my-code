12511061adafba21c34d05e030e2493ba920ecd2
Hugo Marthinet <h.marthinet@gmail.com>
2024-08-13 10:14:16 +0200

    Added some tests

--- 02 - Stats/Mémos Fonctions/Tests.R
+++ 02 - Stats/Mémos Fonctions/Tests.R
@@ -4,7 +4,40 @@
 
 # https://vsp.pnnl.gov/help/vsample/design_trend_mann_kendall.htm
 trend::mk.test()
+Kendall::MannKendall()
 
 #### __2 samples
 
+# 2 numeric samples
 stats::prop.trend.test()
+
+# 1 numeric 1 binary
+DescTools::CochranArmitageTest()
+
+
+#### Proportions ####
+
+####__Count data
+rstatix::mcnemar_test()
+
+
+#### Variances ####
+
+car::leveneTest()
+DescTools::LeveneTest()
+
+stats::bartlett.test()
+
+
+
+
+#### Autocorrelation ####
+
+car::durbinWatsonTest()
+DescTools::DurbinWatsonTest()
+
+
+
+#### Multiple Comparisons ####
+
+DescTools::DunnTest()
