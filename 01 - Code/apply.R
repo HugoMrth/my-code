669d8a8e86accd5e64f0e6a224b8af134dbcc9c5
Hugo Marthinet <h.marthinet@gmail.com>
2024-09-26 11:00:49 +0200

    Update and rename Convert columns types using apply.R to apply.R

--- 01 - Code/apply.R
+++ 01 - Code/apply.R
@@ -1,2 +1,5 @@
 # Conversion de toutes les colonnes integer en numerique
 data[, unlist(lapply(data, is.integer))] <- apply(data[, unlist(lapply(data, is.integer))], 2, as.character)
+
+# element wise max of two vector 
+pmax(x, y)
