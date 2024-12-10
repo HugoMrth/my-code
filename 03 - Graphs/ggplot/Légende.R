87b898373593c258cd400ee10bb3d315a8ca9500
Hugo Marthinet <h.marthinet@gmail.com>
2024-12-10 11:37:18 +0100

    Update Légende.R

--- 03 - Graphs/ggplot/Légende.R
+++ 03 - Graphs/ggplot/Légende.R
@@ -10,3 +10,15 @@ ggplot(data=nb_tlc,
        lty = "Légende")+
   scale_colour_manual(values=c("TLC disp" = "#be123c", "TLC" = "#1e3a8a", "Ratio" = "#64748b")) + 
   scale_linetype_manual(values=c("TLC disp" = 1, "TLC" = 1, "Ratio" = 2))
+
+
+
+
+theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line.x = element_line(colour = "black", size = 0.3),
+        axis.ticks.x = element_line(colour = "black", size = 0.3),
+        axis.line.y = element_line(colour = "black", size = 0.3),
+        axis.ticks.y = element_line(colour = "black", size = 0.3),
+        legend.position = c(0.75, 0.85),
+        legend.background = element_rect(fill = "transparent", color = "white"))
